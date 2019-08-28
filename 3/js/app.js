8ff40a5172de267e3200f3586df7eea499a90677
osama22711 <42546204+osama22711@users.noreply.github.com>
2019-08-28 16:42:25 +0300

    a

---
+++ 3/js/app.js
@@ -0,0 +1,8 @@
+function init(){
+const onlineButton = document.querySelector(".online-button");
+const offlineButton = document.querySelector(".offline-button");
+const navbar = document.querySelector(".navbar");
+offlineButton.addEventListener("click", function() {
+  });
+}
+init();
